d1e742c468c682a8ad7b52c6b04ea8e34d0eb3c1
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-04-02 23:59:51 +0000

    Rspecテスト完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,14 +15,15 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
+    @user = User.find(params[:id])
+    
     if @user.update(user_params)
       redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
 
