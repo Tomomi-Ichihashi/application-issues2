9393a071c2c94a3d8ce182a5953e3a345c141075
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-04-04 12:56:27 +0000

    [Finish]応用課題4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    current_user.books.new(book_prams)
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
