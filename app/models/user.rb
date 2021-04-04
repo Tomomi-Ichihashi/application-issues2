9020e6e6c223cface328c5dad8ba0d5629dc5fe7
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-04-04 05:01:23 +0000

    [Add]usermodel　アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,21 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-   
+  
+  # フォロー・フォロワー機能 #
+  # ---自分がフォローしているユーザーとの関連
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :follwing_id
+  has_many :followings, through: :active_relationships, source: :follower
+  
+  # ---自分がフォローされるユーザーとの関連
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :follwers, through: :passive_relationshis, source: :following
+  
+  def followed_by?(user)
+    passive_relationships.find_by(follwin_id: user.id).present?
+  end
+  # フォロー・フォロワー機能 
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
