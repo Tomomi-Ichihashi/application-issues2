b69d7c95284e0a2b1ed4d2f50411f05dd79832c6
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-04-05 02:16:12 +0000

    [Start]応用課題5

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,42 @@
+class SearchController < ApplicationController
+  def search
+    @model = params["search"]["model"]
+    @value = params["search"]["value"]
+    @hoe = params["search"]["how"]
+    @datas = search_for(@how, @model, @value)
+  end
+  
+  private
+  
+  def match(model, value)
+    if model == 'user'
+      User.where(name: value)
+    elsif model == 'book'
+      Book.where(title: value)
+    end
+  end
+  
+  def forward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "#{value}%")
+    end
+  end
+  
+  def backward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}")
+    elsif model == "book"
+      Book.where("title LIKE ?", "%#{value}")
+    end
+  end
+  
+  def partical(model,value)
+    if model == 'user'
+      User.where("name LIKE ?","%#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?","%%#{value}")
+    end 
+    
+end
